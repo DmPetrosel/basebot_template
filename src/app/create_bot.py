8759f0c0796057a276187b8f3d1b072b6466896f
Dmitriy Petrosel <Dm.Petroselinum@gmail.com>
2025-06-13 07:24:20 +0300

    fix comment

--- src/app/create_bot.py
+++ src/app/create_bot.py
@@ -8,7 +8,7 @@ from config.settings import settings
 from aiogram.fsm.storage.memory import MemoryStorage
 # TODO Use redis for production version. !Note Redis can store ONLY int ans str
 # from aiogram.fsm.storage.redis import Redis, RedisStorage
-redis= Redis()
+# redis= Redis()
 # storage = RedisStorage(redis=redis)
 storage = MemoryStorage()
 tasks = []
